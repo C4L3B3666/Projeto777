3326c995608ba44f70caf18ec4cf9ab3ed2a5467
C4L3B3666 <calebec055@gmail.com>
2025-08-27 22:11:44 +0100

    Finalização da seção 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,8 @@ menu_bar.addEventListener("click", ()=> {
     document.querySelector(".nav_mobile").classList.toggle("clip")
     document.querySelector(".nav_mobile").classList.toggle("clipX")
 })
+
+var ver_regras = document.querySelector(".button_ver_regras")
+ver_regras.addEventListener("click", ()=> {
+    var secao2 = document.getElementById("secao2").classList.toggle("secao2_expandida")
+})
